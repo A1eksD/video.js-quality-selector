ccca06664ec5ea2b3c4b138827144121817ad90d
A1eksD <aleks444@gmx.de>
2025-05-29 22:41:50 +0200

    changed some thing so any

--- projects/videojs-quality-selector/src/lib/videojs-quality-selector.component.ts
+++ projects/videojs-quality-selector/src/lib/videojs-quality-selector.component.ts
@@ -1,65 +1,69 @@
-import {   Component, Input, ViewChild, ElementRef, OnInit, OnDestroy} from '@angular/core';
-import { QualityOption } from './quality-option';
+import {
+  Component, Input, ViewChild, ElementRef, OnInit, OnDestroy
+} from '@angular/core';
+
 import videojs from 'video.js';
+import Player from "video.js/dist/types/player";
 import 'videojs-contrib-quality-levels';
 import 'videojs-http-source-selector';
+import { QualityOption } from './quality-option';
 
 @Component({
   selector: 'lib-videojs-quality-selector',
-  imports: [],
-  styleUrls: ['./quality-selector-selector.component.scss'],
   template: `
     <video #target class="video-js vjs-default-skin" controls></video>
-
     <div class="quality-selector" [class.show-dropdown]="showDropdown">
       <button class="dropdown-toggle" (click)="toggleDropdown()">
         {{ selectedQuality }}
       </button>
-    <div class="dropdown-menu">
-      <button
-        *ngFor="let q of qualities"
-        class="qualityBtn"
-        (click)="selectQuality(q)"
-      >
-        {{ q.label }}
-      </button>
+      <div class="dropdown-menu">
+        <button
+          *ngFor="let q of qualities"
+          class="qualityBtn"
+          (click)="selectQuality(q)"
+        >
+          {{ q.label }}
+        </button>
+      </div>
     </div>
-  </div>
   `,
-  styles: ``
+  styleUrls: ['./quality-selector-selector.component.scss']
 })
-export class VideojsQualitySelectorComponent {
-
+export class VideojsQualitySelectorComponent implements OnInit, OnDestroy {
   @ViewChild('target', { static: true })
   target!: ElementRef<HTMLVideoElement>;
 
   @Input() src!: string;
   @Input() poster?: string;
 
-  player!: videojs.Player;
+  // Typ über ReturnType<typeof videojs> ableiten
+  player!: ReturnType<typeof videojs>;
   qualities: QualityOption[] = [];
   selectedQuality = 'Auto';
   showDropdown = false;
 
   ngOnInit() {
-    // Player initialisieren
-    this.player = videojs(this.target.nativeElement, {
-      sources: [{ src: this.src, type: 'application/x-mpegURL' }],
-      poster: this.poster
-    }, () => {
-      // qualityLevels API aktivieren
-      const levels = this.player.qualityLevels();
-      // vorhandene Levels sammeln
-      for (let i = 0; i < levels.length; i++) {
-        const lvl = levels[i];
-        this.qualities.push({
-          index: i,
-          height: lvl.height,
-          label: `${lvl.height}p`
-        });
+    this.player = videojs(
+      this.target.nativeElement,
+      {
+        sources: [{ src: this.src, type: 'application/x-mpegURL' }],
+        poster: this.poster
+      },
+      () => {
+        // Plugin-Methode über any aufrufen
+        const levels = (this.player as any)
+          .qualityLevels() as Array<{ height?: number; enabled: boolean }>;
+
+        for (let i = 0; i < levels.length; i++) {
+          const lvl = levels[i];
+          this.qualities.push({
+            index: i,
+            height: lvl.height ?? 0,
+            label: `${lvl.height}p`
+          });
+        }
       }
-      // Default bleibt “Auto” (alle Levels enabled)
-    });
+    );
   }
 
   toggleDropdown() {
@@ -67,18 +71,19 @@ export class VideojsQualitySelectorComponent {
   }
 
   selectQuality(q: QualityOption) {
-    const levels = this.player.qualityLevels();
-    // nur das gewählte Level erlauben
+    const levels = (this.player as any)
+      .qualityLevels() as Array<{ enabled: boolean }>;
+
     for (let i = 0; i < levels.length; i++) {
       levels[i].enabled = (i === q.index);
     }
+
     this.selectedQuality = q.label;
     this.showDropdown = false;
   }
 
   ngOnDestroy() {
-    this.player?.dispose();
+    this.player.dispose();
   }
-
 }
 
